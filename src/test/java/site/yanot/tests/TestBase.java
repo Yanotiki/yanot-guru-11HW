97a316372bc66f7df50870e533e3b4f5aa8f8225
Yanotiki <yana.fedorenko@soft-machine.ru>
2022-02-03 23:39:53 +0300

    12. Телеграм-бот. Отправляем уведомления о результатах прохождении автотестов

---
+++ src/test/java/site/yanot/tests/TestBase.java
@@ -0,0 +1,51 @@
+package site.kpokogujl.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import site.kpokogujl.helpers.Attach;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class TestBase {
+    @BeforeAll
+    static void setUp(){
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        String browser = System.getProperty("browser", "chrome");
+        String version = System.getProperty("version", "91");
+        String remoteUrl = System.getProperty("remoteUrl", "selenoid.autotests.cloud/wd/hub");
+        String login = System.getProperty("login", "user1");
+        String pass = System.getProperty("pass", "1234");
+
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.browser = browser;
+        Configuration.browserVersion = version;
+
+        String url = "https://" + login + ":" + pass + "@" + remoteUrl;
+
+        Configuration.remote = url;
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+
+        Attach.attachAsText("Browser: ", browser);
+        Attach.attachAsText("Version: ", version);
+        Attach.attachAsText("Remote Url: ", remoteUrl);
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
+    }
+}
